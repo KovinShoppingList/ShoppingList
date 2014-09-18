60a4a9d967d2c35da8aaea1c7c7fe051f84865d1
EvaSleva <edrewsen@gmail.com>
2014-09-18 09:57:29 +0100

    Nýtt projekt

---
+++ ShoppingList.User/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoppingList.User.Startup))]
+namespace ShoppingList.User
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
